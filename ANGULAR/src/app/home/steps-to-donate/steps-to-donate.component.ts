6d7ca800dd0d9dc75bf504ef6dc941e0989f2995
tobiDosumu <dosunmumishael@gmail.com>
2022-07-23 17:02:31 +0100

    Setting up the project in Angular

---
+++ ANGULAR/src/app/home/steps-to-donate/steps-to-donate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-steps-to-donate',
+  templateUrl: './steps-to-donate.component.html',
+  styleUrls: ['./steps-to-donate.component.scss']
+})
+export class StepsToDonateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
